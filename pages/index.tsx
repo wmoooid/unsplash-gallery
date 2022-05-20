fbac24eb7d345f2ca9a53ebc1604014fedce77b8
wmoooid <threeqletters@gmail.com>
2022-05-20 16:53:04 +0400

    Carousel working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import type { NextPage } from 'next';
 import React from 'react';
 
+const transition_time = 300;
 const card_mb = 125;
 const card_gap = 30;
 const card_width = 285;
 const card_height = 435;
 const card_center = `calc(50vw - ${card_width / 2}px)`;
+const card_hold = {
+  width: card_width,
+  height: card_height,
+  bottom: card_mb,
+  left: card_center,
+  borderRadius: '20px',
+  visivility: 'hidden',
+};
+const card_move = {
+  width: '100%',
+  height: '100%',
+  bottom: 0,
+  left: 0,
+  visivility: 'visible',
+  borderRadius: '0px',
+  transition: `all ${transition_time}ms ease-in-out`,
+};
 
 const Home: NextPage = () => {
   const [list, setList] = React.useState([
@@ -15,18 +33,37 @@ const Home: NextPage = () => {
     { src: 'image-3.jpg' },
     { src: 'image-4.jpg' },
   ]);
+  const [backgroundList, setBackgroundList] = React.useState([list[0], list[1]]);
+  const [nextCardStyle, setNextCardStyle] = React.useState<any>(card_hold);
 
   const handleClick = () => {
+    setNextCardStyle(card_move);
     const newList = [...list];
     newList.shift();
     setList(newList);
+    setTimeout(() => {
+      setBackgroundList([newList[0], newList[1]]);
+      setNextCardStyle(card_hold);
+    }, transition_time);
   };
 
   return (
     <main>
       <section className='container'>
+        <div className='background'>
+          <img
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
+            src={backgroundList[0].src}
+            alt=''
+            className='background-image'
+          />
+          <img style={nextCardStyle} src={backgroundList[1].src} alt='' className='background-image' />
+        </div>
         <ul className='card-list'>
-          {list.map((el, i) => (
+          {list.slice(1).map((el, i) => (
             <li
               key={el.src}
               style={{
